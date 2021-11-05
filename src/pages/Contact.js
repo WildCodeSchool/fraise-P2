115c2ac53092491a0b7b42e41e4f5b232b0a5d09
AnaisBonin <anaisboninfr@gmail.com>
2021-11-05 14:27:30 +0100

    CSS contact page done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import "./Contact.css";
-// import{ init } from 'emailjs-com';
-// init("user_mHtCtPRmfXansncIGdpdq");
+import{ init } from 'emailjs-com';
+init("user_mHtCtPRmfXansncIGdpdq");
 
 const Contact = () => {
   const [lastName, setLastName] = useState("");
